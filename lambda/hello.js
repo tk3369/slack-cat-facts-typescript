3fa4c1f895cf7c466ba583f98d03e24dd409e8ba
Tom Kwong <tk3369@gmail.com>
2019-11-14 07:12:31 -0800

    first commit

---
+++ lambda/hello.js
@@ -0,0 +1,36 @@
+var rp = require('request-promise');
+
+exports.handler = async function(event) {
+  console.log("Request: ", JSON.stringify(event, undefined, 2));
+
+  return await rp({
+    uri: 'https://catfact.ninja/fact',
+    method: 'GET',
+    json: true
+  }).then(function (parsedBody) {
+    console.log('request to catfact.ninja was successful: ', 
+      JSON.stringify(parsedBody, undefined, 2));
+    var result = { 
+      'response_type': 'in_channel',
+      'text': 'Did you know... ' + parsedBody.fact
+    };
+    console.log('result: ', result);
+    var return_val = {
+      statusCode: 200,
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(result)
+    };
+    console.log('return_val: ', return_val);
+    return return_val;
+  })
+  .catch(function (err) {
+    console.log('request to catfact.ninja failed with error: ' + err);
+    return {
+      statusCode: 500,
+      body: 'Sorry, unable to hit catfact api!'
+    };
+  });
+
+}
--- lib/cdk_workshop-stack.ts
+++ lib/cdk_workshop-stack.ts
@@ -1,18 +1,21 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda/deployment.zip'),
+      //code: lambda.Code.asset('lambda'),  // code is loaded from lambda directory
+      handler: 'hello.handler'            // hello.js with handler function
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    new apigw.LambdaRestApi(this, 'Gateway', {
+      handler: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
